90f34ca3830011c10e6dd829ef53d73361529867
samuelwong5 <samuel_c_wong@hotmail.com>
2015-08-17 12:43:58 +0800

    New chat windows for each contact; main gui is contacts list

--- messenger.py
+++ messenger.py
@@ -4,14 +4,71 @@ from socket import *
 import sys
 from Tkinter import *
 
-class MessageApp():
-    def __init__(self, root, queue, port=13000):
-        # initialize gui
-        self.root = root
-        self.root.wm_title("Messenger")
+
+class Messenger():
+    def __init__(self, queue, port=13000):
+        self.main_msg_queue = queue  # Queue for all messages
+        self.port = port             # Port configuration
+        self.contact_dict = {}
+        # self.contact_dict = {'172.27.19.126' : [None, 'TestServer']}
+        
+        # Initialize GUI
+        self.root = Tk()
+        self.root.wm_title('Contacts')
         self.root.resizable(width=FALSE,height=FALSE)
-        self.root.geometry('{}x{}'.format(460,340))
-        self.root.protocol("WM_DELETE_WINDOW", self.exit)
+        self.root.geometry('{}x{}'.format(200,420))
+        Label(self.root, text='Contacts:').place(x=10,y=10)
+        self.contact_lb = Listbox(self.root)
+        self.contact_lb.place(x=10,y=30,height=310,width=180)
+        # self.contact_lb.insert(1, "TestServer")
+        self.contact_lb.bind('<Double-1>', self.open_chat_window)
+        Label(self.root, text='IP:').place(x=10,y=350)
+        Label(self.root, text='Name:').place(x=10,y=370)
+        self.ip_inpt = Entry(self.root)
+        self.ip_inpt.place(x=60,y=350,width=100)
+        self.nm_inpt = Entry(self.root)
+        self.nm_inpt.place(x=60,y=370,width=100)
+        self.ip_butn = Button(self.root, text='Add',command=self.contact_add)
+        self.ip_butn.grid(sticky=W,pady=4)
+        self.ip_butn.place(x=10,y=390,width=150, height=20)   
+        self.root.after(1000, self.receive_message)
+        self.root.mainloop() 
+        
+    def receive_message(self, buff=1024):
+        while not self.main_msg_queue.empty():
+            (data, addr) = self.main_msg_queue.get()
+            try:
+                q = self.contact_dict[addr[0]][0]
+                if q is not None:
+                    q.put((data,addr))
+            except KeyError:
+                pass 
+        self.root.after(1000, self.receive_message)    
+        
+    def open_chat_window(self, event):
+        contact_name = self.contact_lb.selection_get()
+        for k,v in self.contact_dict.iteritems():
+            if v[1] == contact_name:
+                new_queue = Queue()
+                self.contact_dict[k][0] = new_queue
+                MessageChatWindow(Tk(), new_queue, k, contact_name, self.port)
+        
+    def contact_add(self):
+        if self.ip_inpt.get().count('.') == 3:
+            name = self.nm_inpt.get() if self.nm_inpt.get() is not '' else self.ip_inpt.get()
+            self.contact_lb.insert('end', name)
+            self.contact_dict[self.ip_inpt.get()] = [None, name]
+            self.ip_inpt.delete(0, 'end')  
+            self.nm_inpt.delete(0, 'end')    
+
+            
+class MessageChatWindow():
+    def __init__(self, root, queue, addr, name='', port=13000):
+        # Initialize GUI
+        self.root = root
+        self.root.wm_title('Messenger ' + ('- ' + name if name != '' else ''))
+        self.root.resizable(width=FALSE,height=FALSE)
+        self.root.geometry('{}x{}'.format(280,340))
         self.msgs = Text(self.root)
         self.msgs.place(x=10,y=10,height=290,width=260)
         self.msgs.config(state=DISABLED)
@@ -20,40 +77,29 @@ class MessageApp():
         self.inpt.bind('<Return>', self.send)
         self.send_btn = Button(root, text='Send',command=self.send)
         self.send_btn.grid(sticky=W,pady=4)
-        self.send_btn.place(x=220,y=310,width=50, height=20)
-        self.ip_list = Text(self.root)
-        self.ip_list.place(x=300,y=10,height=250,width=150)
-        self.ip_list.config(state=DISABLED)
-        Label(root, text='Name:').place(x=300,y=290)
-        Label(root, text='IP:').place(x=300,y=270)
-        self.ip_inpt = Entry(root)
-        self.ip_inpt.place(x=350,y=270,width=100)
-        self.nm_inpt = Entry(root)
-        self.nm_inpt.place(x=350,y=290,width=100)
-        self.ip_butn = Button(root, text='Add',command=self.add_ip)
-        self.ip_butn.grid(sticky=W,pady=4)
-        self.ip_butn.place(x=300,y=310,width=150, height=20)       
+        self.send_btn.place(x=220,y=310,width=50, height=20)      
         self.count = 0
+        self.name = name if name != '' else addr
         
-        # config udp socket for sending messages
+        # UDP socket for sending messages
         self.send_port = port
-        self.send_addr = {}
+        self.send_addr = addr
         self.send_sock = socket(AF_INET, SOCK_DGRAM)
         self.msg_queue = queue
+        
+        # Callback for receiving messages
+        self.root.after(1000, self.recv)
+        self.root.mainloop()
         
     def recv(self):
         buff = 1024
         while not self.msg_queue.empty():
             (data, addr) = self.msg_queue.get()
             try:
-                self.add_msg(self.msgs, self.send_addr[addr[0]] + ": " + data)
+                self.add_msg(self.msgs, self.name + ": " + data)
             except KeyError:
                 self.add_msg(self.msgs, addr[0] + ": " + data)
         self.root.after(1000, self.recv)
-    
-    def start(self):
-        self.root.after(1000, self.recv)
-        self.root.mainloop()
         
     def add_msg(self, txt_box, text):
         txt_box.config(state=NORMAL)
@@ -65,17 +111,8 @@ class MessageApp():
         
     def send(self, text=''):
         self.add_msg(self.msgs, "YOU: " + self.inpt.get())
-        for addr in self.send_addr.keys():
-            self.send_sock.sendto(self.inpt.get(), (addr, self.send_port))
+        self.send_sock.sendto(self.inpt.get(), (self.send_addr, self.send_port))
         self.inpt.delete(0, 'end')        
-
-    def add_ip(self):
-        if self.ip_inpt.get().count('.') == 3:
-            self.add_msg(self.ip_list, self.nm_inpt.get())
-            name = self.nm_inpt.get() if self.nm_inpt.get() is not '' else self.ip_inpt.get()
-            self.send_addr[self.ip_inpt.get()] = name
-            self.ip_inpt.delete(0, 'end')  
-            self.nm_inpt.delete(0, 'end')
             
     def exit(self):
         sys.exit(0)
@@ -91,9 +128,7 @@ def msg_listen(queue, port=13000):
 
     
 def msgr_init(queue):
-    root = Tk()
-    messenger = MessageApp(root, queue)
-    messenger.start()
+    msgr = Messenger(queue)
   
   
 # ------- Terminal Messenger -------  
